8fb0bb2a443f48cdf6dc31d445aff0471b52e2e2
Yesol <kwonyesol@gmail.com>
2022-01-18 23:53:29 +1000

    drawing basic fuction done

---
+++ app.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+canvas.height = 500;
+canvas.width = 500;
+
+ctx.storokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseEnter(event) {
+  startPainting();
+  ctx.beginPath();
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mouseenter", onMouseEnter);
+}
